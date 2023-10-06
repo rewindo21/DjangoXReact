28627fda67cfbde36d05b1561c77872d1b217592
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2023-10-06 18:10:40 +0330

    spinner and error message for homeScreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect} from 'react'
-import Product from '../components/Product'
-import { Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { listProducts } from '../actions/productActions'
-// import axios from 'axios'
+import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { Row, Col } from 'react-bootstrap'
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
@@ -13,18 +14,22 @@ const HomeScreen = () => {
   useEffect(() => {
     dispatch(listProducts())
 
-  }, [])
+  }, [dispatch])
 
   return (
     <div>
         <h1>Latest Products</h1>
-        <Row>
-            {products.map(product => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                  <Product product={product}/>
-                </Col>
-            ))}
-        </Row>
+        {loading ? <Loader />
+          : error ? <Message variant='danger'>{error}</Message>
+          :
+          <Row>
+              {products.map(product => (
+                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product}/>
+                  </Col>
+              ))}
+          </Row>
+        }
     </div>
   )
 }
