a8b97d978c3f62602c5698961490c68910ff47fc
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2023-10-27 14:15:39 +0330

    user reducer for register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route path='/' element={<HomeScreen />}/>
             <Route path='/login' element={<LoginScreen />}/>
+            <Route path='/register' element={<RegisterScreen />}/>
             <Route path='/product/:id' element={<ProductScreen />}/>
             <Route path='/cart/:id?' element={<CartScreen />}/>
           </Routes>
--- frontend/src/actions/userActions.jsx
+++ frontend/src/actions/userActions.jsx
@@ -3,7 +3,12 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
-    USER_LOGOUT,
+
+    // USER_LOGOUT,
+
+    // USER_REGISTER_REQUEST,
+    // USER_REGISTER_SUCCESS,
+    // USER_REGISTER_FAIL,
 } from '../constants/userConstants' 
 
 export const login = (email, password) => async (dispatch) => {
@@ -45,3 +50,43 @@ export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({ type: USER_LOGOUT })
 }
+// 
+// export const register = (name, email, password) => async (dispatch) => {
+//     try {
+//         dispatch({
+//             type: USER_REGISTER_REQUEST
+//         })
+
+//         const config = {
+//             headers: {
+//                 'Content-type': 'application/json'
+//             }
+//         }
+
+//         const res = await axios.post(
+//             'http://127.0.0.1:8000/api/users/register/',
+//             { 'first_name': name, 'email': email, 'password': password },
+//             config
+//         )
+
+//         dispatch({
+//             type: USER_REGISTER_SUCCESS,
+//             payload: res.data
+//         })
+
+//         dispatch({
+//             type: USER_LOGIN_SUCCESS,
+//             payload: res.data
+//         })
+
+//         localStorage.setItem('userInfo', JSON.stringify(res.data))
+
+//     } catch (error) {
+//         dispatch({
+//             type: USER_REGISTER_FAIL,
+//             payload: error.res && error.res.data.detail
+//                 ? error.res.data.detail
+//                 : error.message,
+//         })
+//     }
+// }
