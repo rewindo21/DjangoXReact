89d408b36833061e2696baf19b65e0c0ce0f99a8
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2023-11-04 22:57:06 +0330

    getUserDetails reducer & action

--- frontend/src/actions/userActions.jsx
+++ frontend/src/actions/userActions.jsx
@@ -1,92 +1,130 @@
-import axios from 'axios'
+import axios from "axios";
 import {
-    USER_LOGIN_REQUEST,
-    USER_LOGIN_SUCCESS,
-    USER_LOGIN_FAIL,
-
-    USER_LOGOUT,
-
-    USER_REGISTER_REQUEST,
-    USER_REGISTER_SUCCESS,
-    USER_REGISTER_FAIL,
-} from '../constants/userConstants' 
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+} from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
-    try {
-        dispatch({
-            type: USER_LOGIN_REQUEST
-        })
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
 
-        const config = {
-            headers: {
-                'Content-type': 'application/json'
-            }
-        }
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
 
-        const res = await axios.post(
-            'http://127.0.0.1:8000/api/users/login/',
-            { 'username': email, 'password': password },
-            config
-        )
+    const res = await axios.post(
+      "http://127.0.0.1:8000/api/users/login/",
+      { username: email, password: password },
+      config
+    );
 
-        dispatch({
-            type: USER_LOGIN_SUCCESS,
-            payload: res.data
-        })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: res.data,
+    });
 
-        localStorage.setItem('userInfo', JSON.stringify(res.data))
-
-    } catch (error) {
-        dispatch({
-            type: USER_LOGIN_FAIL,
-            payload: error.res && error.res.data.detail
-                ? error.res.data.detail
-                : error.message,
-        })
-    }
-}
+    localStorage.setItem("userInfo", JSON.stringify(res.data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.res && error.res.data.detail
+          ? error.res.data.detail
+          : error.message,
+    });
+  }
+};
 
 export const logout = () => (dispatch) => {
-    localStorage.removeItem('userInfo')
-    dispatch({ type: USER_LOGOUT })
-}
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
+};
 
 export const register = (fname, lname, email, password) => async (dispatch) => {
-    try {
-        dispatch({
-            type: USER_REGISTER_REQUEST
-        })
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    });
 
-        const config = {
-            headers: {
-                'Content-type': 'application/json'
-            }
-        }
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
 
-        const res = await axios.post(
-            'http://127.0.0.1:8000/api/users/register/',
-            { 'first_name': fname, 'last_name': lname, 'email': email, 'password': password },
-            config
-        )
+    const res = await axios.post(
+      "http://127.0.0.1:8000/api/users/register/",
+      { first_name: fname, last_name: lname, email: email, password: password },
+      config
+    );
 
-        dispatch({
-            type: USER_REGISTER_SUCCESS,
-            payload: res.data
-        })
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: res.data,
+    });
 
-        dispatch({
-            type: USER_LOGIN_SUCCESS,
-            payload: res.data
-        })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: res.data,
+    });
 
-        localStorage.setItem('userInfo', JSON.stringify(res.data))
+    localStorage.setItem("userInfo", JSON.stringify(res.data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.res && error.res.data.detail
+          ? error.res.data.detail
+          : error.message,
+    });
+  }
+};
 
-    } catch (error) {
-        dispatch({
-            type: USER_REGISTER_FAIL,
-            payload: error.res && error.res.data.detail
-                ? error.res.data.detail
-                : error.message,
-        })
-    }
-}
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`
+      },
+    };
+
+    const res = await axios.get(
+      `http://127.0.0.1:8000/api/users/${id}/`,
+      config
+    );
+
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload:
+        error.res && error.res.data.detail
+          ? error.res.data.detail
+          : error.message,
+    });
+  }
+};
---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,16 @@
+// import React, { useState, useEffect } from "react";
+// import { Link, useLocation, useNavigate } from "react-router-dom";
+// import { Form, Button, Row, Col } from "react-bootstrap";
+// import { useDispatch, useSelector } from "react-redux";
+// import Loader from "../components/Loader";
+// import Message from "../components/Message";
+// import { getUserDetails } from "../actions/userActions";
+
+
+// const ProfileScreen = () => {
+//   return (
+//     <div>ProfileScreen</div>
+//   )
+// }
+
+// export default ProfileScreen
